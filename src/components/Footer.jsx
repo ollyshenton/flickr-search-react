6ebdcfaf72534ed1e27c805c4b801697d0d9b1ca
Olly Shenton <olivershenton@gmail.com>
2021-03-14 15:52:53 +0000

    style footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="container m-auto py-11 border-t-2 border-fuchsia-600 ">
+    <footer className="container m-auto py-11 border-t-2 border-yellow-200">
       <div className="flex flex-row justify-between space-between">
         <p>Powered by Flickr</p>
         <p>⚡ be excellent to each other ⚡</p>
